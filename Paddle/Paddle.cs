bb954cbec9109d10e50bc0e6d3c37f5c0963741f
christiansj <christiansj10@gmail.com>
2026-06-19 20:03:51 -0500

    Initial commit

---
+++ Paddle/Paddle.cs
@@ -0,0 +1,44 @@
+using Godot;
+using System;
+
+public partial class Paddle : Area2D
+{
+	[Export]
+	public int Speed {get; set; } = 400;
+
+	public Vector2 ScreenSize;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		ScreenSize = GetViewportRect().Size;
+		GD.Print(ScreenSize);
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		var velocity = Vector2.Zero;
+
+		if(Input.IsActionPressed("move_right"))
+		{
+			velocity.X += 1;
+		}
+
+		if(Input.IsActionPressed("move_left"))
+		{
+			velocity.X -= 1;
+		}
+
+		if(velocity.Length() > 0)
+		{
+			velocity = velocity.Normalized() * Speed;
+		}
+
+		Position += velocity * (float)delta;
+		Position = new Vector2(
+			x: Mathf.Clamp(Position.X, 0, ScreenSize.X - 100),
+			y: Mathf.Clamp(Position.Y, 0, ScreenSize.Y)	
+		);
+	}
+}
