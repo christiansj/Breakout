abb273268178d8d314e81eb93c2923afba09d28d
christiansj <christiansj10@gmail.com>
2026-06-20 19:18:27 -0500

    ball movement

--- Ball/Ball.cs
+++ Ball/Ball.cs
@@ -4,13 +4,13 @@ using System;
 public partial class Ball : CharacterBody2D
 {
     [Export]
-	public int Speed { get; set; } = 300;
+	public int Speed { get; set; } = 350;
 
 	public Vector2 ScreenSize;
 
     public override void _Ready()
 	{
-		Velocity = new Vector2(-100, 200).Normalized() * Speed;
+		Velocity = new Vector2(-200, 200).Normalized() * Speed;
 	}
     
     public override void _PhysicsProcess(double delta)
@@ -18,7 +18,26 @@ public partial class Ball : CharacterBody2D
         KinematicCollision2D collision = MoveAndCollide(Velocity * (float)delta);
         if (collision != null)
         {
-            GD.Print("COLLIDED");
+            if(collision.GetCollider().GetClass() == "CharacterBody2D")
+            {
+                float paddleX = (collision.GetCollider() as Node2D).Position.X;
+                float collisionX = collision.GetPosition().X;
+            
+                if(collisionX > paddleX+60)
+                {
+                    if(Velocity.X < 0)
+                    {
+                        Velocity *= -1;
+                    }
+                }
+                else
+                {
+                    if(Velocity.X > 0)
+                    {
+                        Velocity *= -1;
+                    }
+                }
+            }
             var reflect = collision.GetRemainder().Bounce(collision.GetNormal());
             Velocity = Velocity.Bounce(collision.GetNormal());
             MoveAndCollide(reflect);
